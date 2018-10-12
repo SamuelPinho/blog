cd9a7029b5b6d84af82ad22137814c70dbda9f4b
SamuelPinho <samuel.pinho@fatec.sp.gov.br>
2018-10-12 17:21:11 +0000

    Change Categories And Tags Component to be Responsive

--- src/components/shared/CategoriesAndTags.js
+++ src/components/shared/CategoriesAndTags.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
 const CategoriesAndTags = ({ data }) => (
-    <nav className="level is-mobile">
-        <div className="level-left">
-            <div className="tags postpreview-tags">
-                {data.frontmatter.categorias.map((tag, index) => (
-                    <span className="tag is-info is-medium" key={index}>{tag}</span>
-                ))}
-                {data.frontmatter.tags.map((tag, index) => (
-                    <span className="tag is-medium" key={index}>{tag}</span>
-                ))}
-            </div>
-        </div>
-    </nav>
+    <div className="tags postpreview-tags">
+        {data.frontmatter.categorias.map((tag, index) => (
+            <span className="tag is-info is-medium" key={index}>{tag}</span>
+        ))}
+        {data.frontmatter.tags.map((tag, index) => (
+            <span className="tag is-medium" key={index}>{tag}</span>
+        ))}
+    </div>
 );
 
 export default CategoriesAndTags;
